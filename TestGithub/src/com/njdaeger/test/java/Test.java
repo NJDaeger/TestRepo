ea97f50f99285df4bbf0fe042ab3fb406bcedde1
NJDaeger <noahdaeger@hotmail.com>
2016-11-10 16:14:13 -0500

    Test

---
+++ TestGithub/src/com/njdaeger/test/java/Test.java
@@ -0,0 +1,16 @@
+package com.njdaeger.test.java;
+
+public class Test extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public Test() {
+		super("Just testing this repo stuff.");
+	}
+	
+	
+
+}
